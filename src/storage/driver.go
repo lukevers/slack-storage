f4938ac3e9267ec405ab45164dfd11d2d1ca5131
Luke Evers <me@lukevers.com>
2016-02-18 18:15:57 -0500

    New storage func's dsn is not multiple arguments.
    I thought it might be when I started writing the driver interface,
    but changed my mind.

--- src/storage/driver.go
+++ src/storage/driver.go
@@ -2,7 +2,7 @@
 package storage
 
 type Storage interface {
-	New(...string) (Storage, error)
+	New(string) (Storage, error)
 	Get(string) (string, error)
 	GetAll() (map[string]string, error)
 	Put(string, string) error
