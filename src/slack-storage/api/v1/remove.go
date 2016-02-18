88e872956fc7a98b222568871760f8ef092ae039
Luke Evers <me@lukevers.com>
2016-02-17 19:15:27 -0500

    Add support for remove in api

--- src/slack-storage/api/v1/remove.go
+++ src/slack-storage/api/v1/remove.go
@@ -6,5 +6,14 @@ import (
 )
 
 func remove(data *slack.Slack, args []string) (int, string, error) {
-	return 200, "", nil
+	if len(args) < 1 {
+		return 400, "", errors.New("Not enough arguments to remove an item.")
+	}
+
+	err := Storage.Remove(args[1])
+	if err != nil {
+		return 400, "", err
+	}
+
+	return 200, "Item removed.", nil
 }
